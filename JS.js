ea3040e6b8b31a12494c3aa36589677ab89a76a7
sqdqdwd <mohaned.menem@gmail.com>
2023-05-02 16:10:27 +0300

    A1

--- JS.js
+++ JS.js
@@ -1,3 +1,21 @@
-hints
-1- testing purposes.  = console.log();
-2- 
+
+var number1 =document.getElementById("num1");
+var number2=document.getElementById("num2");
+var result=document.getElementById("result");
+function add(){
+  var num1=Number(number1.value);
+  var num2=Number(number2.value);
+  result.classList.remove("d-none")
+   result.innerHTML=num1+num2;
+}
+function subtract(){
+  var num1=Number(number1.value);
+  var num2=Number(number2.value)
+  result.classList.remove("d-none")
+   result.innerHTML=num1-num2;
+}
+function clearInput(){
+  number1.value="";
+  number2.value="";
+  result.classList.add("d-none")
+}
